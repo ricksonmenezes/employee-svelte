944e235e9871d69ed56de67eaaaf97f2640160c7
ricksonmenezes <ricksonmenezes@gmail.com>
2024-08-03 14:03:28 +0800

    added login logout functionality.

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,95 @@
-<script>import "./app.css";
-import svelteLogo from './assets/svelte.svg'
-import viteLogo from '/vite.svg'
-import Counter from './lib/Counter.svelte'
-import { Router, Route, Link, navigate } from 'svelte-routing';
-/*import Router from 'svelte-spa-router';*/
-import EmployeeList from "./routes/EmployeeList.svelte";
-import AddEmployee from "./routes/AddEmployee.svelte";
-import ViewEmployee from "./routes/ViewEmployee.svelte";
-import EditEmployee from "./routes/EditEmployee.svelte";
-</script>
+  <script>
+    import "./app.css";
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import { Router, Route, navigate } from 'svelte-routing';
+  import EmployeeList from "./routes/EmployeeList.svelte";
+  import AddEmployee from "./routes/AddEmployee.svelte";
+  import ViewEmployee from "./routes/ViewEmployee.svelte";
+  import EditEmployee from "./routes/EditEmployee.svelte";
+  //import SignIn from "./routes/SignIn.svelte";
+  import {userbaseStore, userStore, promiseStore} from './stores'
+    import SignIn from "./routes/SignIn.svelte";
+    import Logout from "./routes/Logout.svelte";
 
-<main>
+  const userbase = window.userbase
+  window.userbase = null
 
-  <div>
+  $userbaseStore = userbase
+  $userStore = null
 
-    <!--<Router routes={{
-                '/': EmployeeList,
-                '/add2': AddEmployee
-            }} />-->
-    <Router>
-      <Route path="/" component={EmployeeList} />
-      <Route path="/add2" component={AddEmployee} />
-      <Route path="/view" component={ViewEmployee} />
-      <Route path="/delete" component={ViewEmployee} />
-      <Route path="/edit" component={EditEmployee} />
-
-    </Router>
-  </div>
-</main>
+    $promiseStore = userbase.init({appId: 'fc0fb7c0-7f0c-4d7c-a1a2-81f1101052ef'})
+          .then((session) => $userStore = session.user)
 
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+
+  function retrySignIn() {
+
+    navigate('/');
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+
+    $: if ($userStore) {
+      console.log('reaches $:' + new Date())
+      navigate('/list');
+    } else {
+      navigate('/');
+    }
+    function signout() {
+      $promiseStore = $userbaseStore.signOut().then(() => $userStore = null)
+    }
+  </script>
+
+  <main>
+
+    <div>
+
+      {#await $promiseStore.then(() => Promise.reject())}
+        Loading..
+      {:catch error}
+        {#if error}
+          debugger
+          <strong class="text-red-700 font-bold">ERROR! {error} </strong>
+        {/if}
+        {#if $userStore}
+          <button on:click={signout}>Logout</button>
+
+        {:else}
+
+        {/if}
+      {/await}
+
+      </div>
+        <div>
+
+      <Router>
+        <Route path="/" component={SignIn} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/list" component={EmployeeList} />
+        <Route path="/add2" component={AddEmployee} />
+        <Route path="/view" component={ViewEmployee} />
+        <Route path="/delete" component={ViewEmployee} />
+        <Route path="/edit" component={EditEmployee} />
+
+      </Router>
+    </div>
+  </main>
+
+
+  <style>
+    .logo {
+      height: 6em;
+      padding: 1.5em;
+      will-change: filter;
+      transition: filter 300ms;
+    }
+    .logo:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+    }
+    .logo.svelte:hover {
+      filter: drop-shadow(0 0 2em #ff3e00aa);
+    }
+    .read-the-docs {
+      color: #888;
+    }
+  </style>
---
+++ src/routes/Logout.svelte
@@ -0,0 +1,14 @@
+
+<script>
+
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div class="bg-white p-8 rounded-lg shadow-md">
+        <h1 class="text-3xl font-bold mb-4">Logged Out</h1>
+        <p class="mb-6">You have been logged out. Please login again.</p>
+        <a href="/"  class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            Login
+        </a>
+    </div>
+</div>
