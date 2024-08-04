85a4f2600298058c00b3a98a30be7ed16ef2aed6
ricksonmenezes <ricksonmenezes@gmail.com>
2024-08-04 10:18:50 +0800

    commented pagination that is not working yet

--- src/routes/EmployeeList.svelte
+++ src/routes/EmployeeList.svelte
@@ -160,7 +160,7 @@
 
             </tbody>
         </table>
-        <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
+        <!--<nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
             <span class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span class="font-semibold text-gray-900 dark:text-white">1-10</span> of <span class="font-semibold text-gray-900 dark:text-white">1000</span></span>
             <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
                 <li>
@@ -185,7 +185,7 @@
                     <a href="/" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
                 </li>
             </ul>
-        </nav>
+        </nav>-->
     </div>
 
     {/if}
